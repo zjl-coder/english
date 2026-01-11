627c68ffce1a7253934b94390790784652e7119c
zjl-coder <1301034106@qq.com>
2026-01-11 21:25:54 +0800

    feat: update

--- src/.vuepress/menu.js
+++ src/.vuepress/menu.js
@@ -66,6 +66,171 @@ const menu = [
     link: '/pet_shop/',
   },
   {
+    text: '3500 vocabularies',
+    children: [
+      {
+        text: '1. Fall in Love with English',
+        link: '/3500_vocabularies/1.md/',
+      },
+      {
+        text: '2. Different Countries Have Different Kinds of Englishes',
+        link: '/3500_vocabularies/2.md/',
+      },
+      {
+        text: '3. A Hard Trip',
+        link: '/3500_vocabularies/3.md/',
+      },
+      {
+        text: '4. A Horrible Earthquake',
+        link: '/3500_vocabularies/4.md/',
+      },
+      {
+        text: '5. The Great President',
+        link: '/3500_vocabularies/5.md/',
+      },
+      {
+        text: '6. A Brave Maid',
+        link: '/3500_vocabularies/6.md/',
+      },
+      {
+        text: '7. A Fair Competition',
+        link: '/3500_vocabularies/7.md/',
+      },
+      {
+        text: '8. Computers',
+        link: '/3500_vocabularies/8.md/',
+      },
+      {
+        text: '9. Wildlife Protection',
+        link: '/3500_vocabularies/9.md/',
+      },
+      {
+        text: '10. My First Band',
+        link: '/3500_vocabularies/10.md/',
+      },
+      {
+        text: '11. An Interesting Festival',
+        link: '/3500_vocabularies/11.md/',
+      },
+      {
+        text: '12. Balanced Diet',
+        link: '/3500_vocabularies/12.md/',
+      },
+      {
+        text: '13. Sailing Home',
+        link: '/3500_vocabularies/13.md/',
+      },
+      {
+        text: '14. Explore MKII',
+        link: '/3500_vocabularies/14.md/',
+      },
+      {
+        text: '15. A Journey across Canada',
+        link: '/3500_vocabularies/15.md/',
+      },
+      {
+        text: '16. A Woman Doctor - Lina',
+        link: '/3500_vocabularies/16.md/',
+      },
+      {
+        text: '17. A New Farming Way',
+        link: '/3500_vocabularies/17.md/',
+      },
+      {
+        text: '18. Chaplin - A Great Master of Humour',
+        link: '/3500_vocabularies/18.md/',
+      },
+      {
+        text: '19. A Misunderstanding',
+        link: '/3500_vocabularies/19.md/',
+      },
+      {
+        text: '20. A Unique Theme Park',
+        link: '/3500_vocabularies/20.md/',
+      },
+      {
+        text: '21. A Strange Disease',
+        link: '/3500_vocabularies/21.md/',
+      },
+      {
+        text: '22. Sightseeing in the United Kingdom',
+        link: '/3500_vocabularies/22.md/',
+      },
+      {
+        text: '23. An Air Crash',
+        link: '/3500_vocabularies/23.md/',
+      },
+      {
+        text: '24. An Amateur Journalist',
+        link: '/3500_vocabularies/24.md/',
+      },
+      {
+        text: '25. First Aid',
+        link: '/3500_vocabularies/25.md/',
+      },
+      {
+        text: '26. Abstract Art',
+        link: '/3500_vocabularies/26.md/',
+      },
+      {
+        text: '27. A Russian Poem',
+        link: '/3500_vocabularies/27.md/',
+      },
+      {
+        text: "28. Let's Help Adolescents",
+        link: '/3500_vocabularies/28.md/',
+      },
+      {
+        text: '29. Refresh Our Environment',
+        link: '/3500_vocabularies/29.md/',
+      },
+      {
+        text: '30. A Volcano Eruption',
+        link: '/3500_vocabularies/30.md/',
+      },
+      {
+        text: '31. Defeat Disability',
+        link: '/3500_vocabularies/31.md/',
+      },
+      {
+        text: '32. Happy Marriage Needs Fighting For',
+        link: '/3500_vocabularies/32.md/',
+      },
+      {
+        text: '33. Mark and a Seal',
+        link: '/3500_vocabularies/33.md/',
+      },
+      {
+        text: '34. Helping the Remote Village',
+        link: '/3500_vocabularies/34.md/',
+      },
+      {
+        text: '35. Study in America',
+        link: '/3500_vocabularies/35.md/',
+      },
+      {
+        text: '36. Immigrate to America',
+        link: '/3500_vocabularies/36.md/',
+      },
+      {
+        text: '37. Be Careful about Cloning',
+        link: '/3500_vocabularies/37.md/',
+      },
+      {
+        text: '38. Strange Patent Applications',
+        link: '/3500_vocabularies/38.md/',
+      },
+      {
+        text: '39. Pronunciation and Status',
+        link: '/3500_vocabularies/39.md/',
+      },
+      {
+        text: '40. A Sly Murder',
+        link: '/3500_vocabularies/40.md/',
+      },
+    ],
+  },
+  {
     text: 'predicate',
     link: '/predicate/',
     children: [
